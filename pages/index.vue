24860197505c6cca4e976afbae11e93981ed4aa9
sitedoed <ed@sitedoed.com.br>
2022-07-02 14:58:33 -0300

    Reinstall

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,42 @@
 <template>
-  <div class="container">
+  <div class="p-8">
+    <h1 class="">Skore-io Challenge</h1>
     <div class="">
-      <h1>Skore-io/Challenge Home</h1>
-      <div class="">
-        <ul>
-          <li v-for="content in contents" :key="content.id" class="py-2">
-            <nuxt-link :to="content.id" class="">
-              {{ content.title }}:
-            </nuxt-link>
-          </li>
-        </ul>
-        <div class="">
-          <nuxt-child :key="$route.params.id"></nuxt-child>
-        </div>
-      </div>
+      <section>
+        <article
+          v-for="item in contents"
+          :key="item.id"
+          class=""
+        >
+          <h2 class="font-bold">{{ item.title }}</h2>
+          <div class="">
+            <p>{{ item.description }}</p>
+            <p><strong>Tipo: </strong> {{ item.type }}</p>
+            <p>Link: {{ item.link }}</p>
+            <p>Embeddable: {{ item.embeddable }}</p>
+            <p>Download: {{ item.allow_download }}</p>
+            <p>Data de criação: {{ item.created_at }}</p>
+            <p>Última atualização: {{ item.updated_at }}</p>
+          </div>
+          <NuxtLink
+            :to="`/item/${item.id}`"
+            class=""
+          >
+            Read more
+          </NuxtLink>
+        </article>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag'
+
 export default {
-  name: 'IndexPage',
   apollo: {
     contents: gql`
-      query getContent {
+      query contents {
         contents {
           id
           title
@@ -35,17 +47,9 @@ export default {
           allow_download
           created_at
           updated_at
-          __typename
         }
       }
     `,
   },
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-}
-</style>
